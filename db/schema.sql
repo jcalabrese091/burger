b096aaf4795f1cebc6c053ef66b7ef43f237af70
Joseph Calabrese <joseph091@dhcp-v052-201.mobile.uci.edu>
2017-09-07 10:09:05 -0700

    make it better with heroku

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	burger_name varchar(255) NOT NULL,
+	devoured Boolean DEFAULT FALSE,
+	at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Texas", 0);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", 0);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("California Burger", 0);
